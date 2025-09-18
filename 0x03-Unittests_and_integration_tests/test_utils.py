54e4d654397e0c1a3baa1e1a2c0f18bba125323f
wamamili <wamamiliclean@gmail.com>
2025-09-18 20:18:08 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map"""
+"""Unit tests for utils.access_nested_map exceptions"""
 
 import unittest
 from parameterized import parameterized
@@ -10,12 +10,13 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test case for access_nested_map"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b'),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map, path, expected_msg):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{expected_msg}'")
 
 
 if __name__ == "__main__":
