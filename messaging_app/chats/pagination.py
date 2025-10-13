7a69c087aab633069630d5c1666fffd1c0771ff2
wamamili <wamamiliclean@gmail.com>
2025-10-13 21:10:29 +0300

    Pagination and Filtering

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,9 +1,22 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     """
-    Pagination class to limit messages to 20 per page.
+    Custom pagination for Messages
+    - 20 messages per page by default
+    - includes total count and page info in response
     """
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
